cdf121be3d3a317f722411bebca33cbdcc6e74b8
Rahul <rimosonu0@gmail.com>
2021-02-18 18:42:36 +0530

    first update

---
+++ index.js
@@ -0,0 +1,26 @@
+const todoInput = document.querySelector("#input");
+const todoButton = document.querySelector(".todo-button");
+const todoList = document.querySelector(".todo-list");
+todoButton.addEventListener("click",addTodo);
+function addTodo(event){
+    event.preventDefault();
+    const todoDiv=document.createElement("div");
+    todoDiv.classList.add("todo");
+    const newTodo=document.createElement('li');
+    newTodo.innerText =todoInput.value;
+    newTodo.classList.add('todo-item');
+    todoDiv.appendChild(newTodo);
+    const completedButton = document.createElement('button');
+    completedButton.innerHTML = '<i class="fas fa-check"></i>';
+    completedButton.classList.add("complete-btn"); 
+    completedButton.textContent="complete";
+    todoDiv.appendChild(completedButton);
+    const trashButton = document.createElement('button');
+    trashButton.innerHTML = '<i class="fas fa-trash"></i>';
+    trashButton.classList.add("complete-btn"); 
+    trashButton.textContent="cancel";
+    todoDiv.appendChild(trashButton);
+    todoList.appendChild(todoDiv);
+    todoInput.value="";    
+}
+
